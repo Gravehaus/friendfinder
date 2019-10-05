7363e91bfe2a60c19fe0c223f36172a96c6f3ec6
juliagraves <gravehaus@gmail.com>
2019-10-05 15:47:03 -0700

    added value strings to friends

--- app/data/pokemon.js
+++ app/data/pokemon.js
@@ -8,61 +8,61 @@ const pokemon = [
   {
     name: "Alcremie",
     photo:"https://cdn.bulbagarden.net/upload/4/4d/Alcremie.png", //insert image here
-    scores: [] //
+    scores: [2,2,4,5,8,5,2,3,8,8] //
   },
 
   {
     name: "Corviknight",
     photo:"https://cdn.bulbagarden.net/upload/thumb/2/2e/Corviknight.png/1200px-Corviknight.png", //insert image here
-    scores: []
+    scores: [9,2,4,5,8,9,3,2,1,3]
   },
 
   {
     name: "Cramorant",
     photo:"https://cdn.bulbagarden.net/upload/9/91/Cramorant.png", //insert image here
-    scores: [] //hp,
+    scores: [3,6,5,6,6,8,8,1,2,4] //hp, attack, defense, spec def, spec attack, speed, agility, evolution, kindness, happiness
   },
 
   {
     name: "Drednaw",
     photo:"https://cdn.bulbagarden.net/upload/thumb/4/4e/Drednaw.png/1200px-Drednaw.png", //insert image here
-    scores: []
+    scores: [8, 5, 4, 3, 1, 9, 2, 6, 7,1]
   },
 
   {
     name: "Duraludon",
     photo:"https://cdn.bulbagarden.net/upload/thumb/2/28/Duraludon.png/1200px-Duraludon.png", //insert image here
-    scores: []
+    scores: [8, 5, 1, 2, 3, 4, 6, 9, 7,9]
   },
 
   {
     name: "Gossifleur",
     photo:"https://cdn.bulbagarden.net/upload/thumb/8/83/Gossifleur.png/1200px-Gossifleur.png", //insert image here
-    scores: []
+    scores: [2, 4, 8, 3, 9, 5, 6, 7, 1,9]
   },
 
   {
     name: "Sirfetch'd",
     photo:"https://cdn.bulbagarden.net/upload/thumb/0/0e/Sirfetch%27d.png/1200px-Sirfetch%27d.png", //insert image here
-    scores: []
+    scores: [3, 6, 2, 9, 5, 8, 4, 1, 7,5]
   },
 
   {
     name: "Sobble",
     photo:"https://cdn.bulbagarden.net/upload/thumb/9/9b/816Sobble.png/1200px-816Sobble.png", //insert image here
-    scores: []
+    scores: [2, 9, 5, 8, 6, 3, 1, 4, 7,2]
   },
 
   {
     name: "Wooloo",
     photo:"https://cdn.bulbagarden.net/upload/thumb/2/2f/Wooloo.png/1200px-Wooloo.png", //insert image here
-    scores: []
+    scores: [4, 8, 1, 2, 7, 5, 6, 3, 9,9]
   },
 
   {
     name: "Zamazenta",
     photo:"https://cdn.bulbagarden.net/upload/thumb/b/bd/Zamazenta.png/1200px-Zamazenta.png", //insert image here
-    scores: []
+    scores: [9, 9, 5, 3, 6, 1, 2, 8, 4,6]
   },
 
 
