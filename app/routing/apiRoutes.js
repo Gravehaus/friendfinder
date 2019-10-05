cb57a1c90ab34a4912e6f12653ef5347988d84f8
juliagraves <gravehaus@gmail.com>
2019-10-05 16:37:58 -0700

    altered 'friends' to pokemon for clarity

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,53 +1,58 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-const tableData = require("../data/pokemon");
+// The apiRoutes.js file includes two basic routes for app.get function and app.post function which used for displaying a JASON data and incoming survey results of all possible pokemon:
+// The app.post(in the apiRoutes.js) used to handle the compatibility logic.
 
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+var pokemon = require('../data/pokemon.js');
 
-module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-  app.get("/api/pokemon", function(req, res) {
-    res.json(tableData);
+// Routing the apiRoutes with the app.get and app.post functions
+module.exports = function (app) {
+  // The app.get request handles when user 'visits' a page
+  app.get('/api/pokemon', function (req, res) {
+    res.json(pokemon);
   });
+  // The app.post request handles when a user submits a form and thus submits data to the surver
+  app.post('/api/pokemon', function (req, res) {
+    // loop through all of the possible options
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+    // To take the result of the user's survey POST and parse it
+    var userData = req.body;
+    var userScores = userData.scores;
+    // To take the results of the user's name and photo, other than the survey questions, to post and parse it
+    var userName = userData.name;
+    var userPhoto = userData.photo;
 
-  app.post("/api/pokemon", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
+    // The variable used to calculate the difference b/n the user's socres and the scores of each user
+    var totalDifference = 0;
+
+    //loop through the pokemon data array of objects to get each pokemon scores
+    for (var i = 0; i < pokemon.length - 1; i++) {
+      console.log(pokemon[i].name);
+      totalDifference = 0;
+
+      //loop through that pokemon score and the users score and calculate the absolute difference between the two and push that to the total difference variable set above
+      for (var j = 0; j < 10; j++) {
+        // We calculate the difference between the scores and sum them into the totalDifference
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(pokemon[i].scores[j]));
+        // If the sum of differences is less then the differences of the current "best match"
+        if (totalDifference <= bestMatch.friendDifference) {
+
+          // Reset the bestMatch to be the new friend.
+          bestMatch.name = pokemon[i].name;
+          bestMatch.photo = pokemon[i].photo;
+          bestMatch.friendDifference = totalDifference;
+        }
+      }
     }
 
-  });
+    // The push method use to save user's data to the database
+    pokemon.push(userData);
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    res.json({ ok: true });
+    //The res.json method will return a JSON data with the user's match which was looped through frieds data array.
+    res.json(bestMatch);
   });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,22 +9,22 @@ var path = require("path");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/pokemon", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-
-  app.get("/survey", function(req, res) {
+  app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
+  /*app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  });*/
+
   // If no matching route is found default to home
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
