801a774216aff13f60466038f177553fe0439c4e
juliagraves <gravehaus@gmail.com>
2019-10-06 00:52:11 -0700

    changed file paths

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -19,12 +19,12 @@ module.exports = function (app) {
   // ---------------------------------------------------------------------------
 
   app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/survey.html"));
+    res.sendFile(path.join(__dirname, "./public/survey.html"));
   });
 
 
   // If no matching route is found default to home
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/home.html"));
+    res.sendFile(path.join(__dirname, "./public/home.html"));
   });
 };
